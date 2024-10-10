d48ffbd447c9c921b34f17fd0a3d2931f99ef878
Andres Martinez <amartinez@goldstar.com>
2024-10-10 14:46:02 -0300

    first commit

---
+++ swiftui-habits/swiftui-habitsTests/swiftui_habitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_habitsTests.swift
+//  swiftui-habitsTests
+//
+//  Created by andres martinez on 10/10/2024.
+//
+
+import Testing
+@testable import swiftui_habits
+
+struct swiftui_habitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
